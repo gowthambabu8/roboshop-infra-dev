11cb203fd4d9946cfb0e87a119e65519f21a3abc
Gowtham <gowthambabu8@gmail.com>
2026-03-06 23:12:18 +0530

    terraform code changes

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,2 +1,9 @@
 #!bin/bash
 dnf install ansible -y
+
+component=$1
+cd /home/ec2-user/
+git clone https://github.com/gowthambabu8/roboshop-ansible-roles-tf.git
+cd roboshop-ansible-roles-tf
+
+ansible-playbook -e component=$component roboshop.yaml
