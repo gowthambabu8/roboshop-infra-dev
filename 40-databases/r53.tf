606695c37482815a801b40f0ab147f3c795fbcdb
Gowtham <gowthambabu8@gmail.com>
2026-03-07 21:57:57 +0530

    terraform code changes

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "mongo" {
   zone_id = var.zone_id
-  name="mongo-${var.environment}-${var.domain_name}"
+  name="mongo-${var.environment}.${var.domain_name}"
   type = "A"
   ttl = "1"
   records = [ aws_instance.mongo.private_ip ]
