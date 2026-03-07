af4bc1e97c51be3486b71497bcbcf71411812d51
Gowtham <gowthambabu8@gmail.com>
2026-03-07 21:54:33 +0530

    terraform code changes

---
+++ 40-databases/r53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "mongo" {
+  zone_id = var.zone_id
+  name="mongo-${var.environment}-${var.domain_name}"
+  type = "A"
+  ttl = "1"
+  records = [ aws_instance.mongo.private_ip ]
+}
--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z04536392HCJLZT52Z8K0"
+}
+
+variable "domain_name" {
+  default = "happielearning.com"
+}
