da6de8d5f3f9964eb751fc3d479a50e18d4be86d
Gowtham <gowthambabu8@gmail.com>
2026-03-09 12:45:29 +0530

    terraform ansible mysql changes

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -6,10 +6,6 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
 
-data "aws_ssm_parameter" "mongo_sg_id" {
-  name = "/${var.project}/${var.environment}/mongo_sg_id"
-}
-
 data "aws_ssm_parameter" "user_sg_id" {
   name = "/${var.project}/${var.environment}/user_sg_id"
 }
@@ -25,4 +21,12 @@ data "aws_ssm_parameter" "redis_sg_id" {
 
 data "aws_ssm_parameter" "mysql_sg_id" {
   name = "/${var.project}/${var.environment}/mysql_sg_id"
+}
+
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
+}
+
+data "aws_ssm_parameter" "mongo_sg_id" {
+  name = "/${var.project}/${var.environment}/mongo_sg_id"
 }
