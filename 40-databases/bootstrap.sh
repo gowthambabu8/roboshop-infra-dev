78f5147324649e5d7ad9597fbc59f17c08fdc31c
Gowtham <gowthambabu8@gmail.com>
2026-03-09 11:22:15 +0530

    terraform ansible mysql changes

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -5,5 +5,5 @@ component=$1
 cd /home/ec2-user/
 git clone https://github.com/gowthambabu8/roboshop-ansible-roles-tf.git
 cd roboshop-ansible-roles-tf
-
+git pull
 ansible-playbook -e component=$component roboshop.yaml
