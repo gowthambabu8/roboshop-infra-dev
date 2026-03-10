8d949caeb1d0bf8d1a69d1eaa399730762f1257a
Gowtham <gowthambabu8@gmail.com>
2026-03-10 15:35:21 +0530

    terraform code changes

--- 20-sg-rules/backend_alb.tf
+++ 20-sg-rules/backend_alb.tf
@@ -6,3 +6,12 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.backend_alb_sg_id
 }
+
+resource "aws_security_group_rule" "backend_alb_bastion_http" {
+  type = "ingress"
+  from_port = "80"
+  to_port = "80"
+  protocol = "tcp"
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.backend_alb_sg_id
+}
