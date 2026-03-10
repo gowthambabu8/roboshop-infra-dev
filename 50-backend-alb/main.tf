3d4add08ce2cddacf3b6e3fa1a53e0399b3c004f
Gowtham <gowthambabu8@gmail.com>
2026-03-10 15:36:32 +0530

    terraform code changes

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -16,7 +16,7 @@ resource "aws_lb_listener" "front_end" {
 
     fixed_response {
       content_type = "text/plain"
-      message_body = "Fixed response content"
+      message_body = "I am from Backend ALB"
       status_code  = "200"
     }
   }
@@ -24,7 +24,7 @@ resource "aws_lb_listener" "front_end" {
 
 resource "aws_route53_record" "backend_alb" {
   zone_id = var.zone_id
-  name = "*.backend_alb-${var.environment}.${var.domain_name} "
+  name = "*.backend_alb-${var.environment}.${var.domain_name}"
   type = "A"
 
   alias {
---
+++ 50-backend-alb/output.tf
@@ -0,0 +1,3 @@
+output "backend_alb" {
+  value = aws_route53_record.backend_alb
+}
