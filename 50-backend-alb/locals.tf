187fa57034985dd61140104506660a2f973e2401
Gowtham <gowthambabu8@gmail.com>
2026-03-10 15:13:31 +0530

    terraform code changes

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -6,5 +6,5 @@ locals {
     Terraform = true
   }
   private_subnet = data.aws_ssm_parameter.private_subnets_id.value
-  backend_alb_sg_id = split(",",data.aws_ssm_parameter.backend_alb_sg_id)
+  backend_alb_sg_id = split(",",data.aws_ssm_parameter.backend_alb_sg_id.value)
 }
