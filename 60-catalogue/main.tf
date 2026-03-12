086cbd8a96df4622cf66ee71ebe228f7fc88df7f
Gowtham <gowthambabu8@gmail.com>
2026-03-12 17:38:07 +0530

    terraform catalogue changes

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -151,7 +151,7 @@ resource "aws_autoscaling_group" "catalogue" {
 }
 
 resource "aws_autoscaling_policy" "catalogue" {
-  autoscaling_group_name = aws_autoscaling_policy.catalogue.name
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
   name = "${var.project}-${var.environment}-catalogue"
   policy_type = "TargetTrackingScaling"
 
