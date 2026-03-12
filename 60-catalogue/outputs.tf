5b56a66b8200d8873960b7453352fe38f93dc3e1
Gowtham <gowthambabu8@gmail.com>
2026-03-12 19:20:45 +0530

    terraform catalogue changes

--- 60-catalogue/outputs.tf
+++ 60-catalogue/outputs.tf
@@ -2,3 +2,7 @@ output "private_subnet" {
   value = data.aws_ssm_parameter.private_subnet_id.value
   sensitive = true
 }
+
+output "catalogue_sg_id" {
+  value = data.aws_ssm_parameter.catalogue_sg_id.value
+}
