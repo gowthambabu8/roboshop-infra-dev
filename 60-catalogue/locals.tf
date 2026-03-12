50dce64b83d5d65f7cc04745f804d39f7dc87baa
Gowtham <gowthambabu8@gmail.com>
2026-03-12 12:25:25 +0530

    terraform catalogue changes

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -5,7 +5,7 @@ locals {
     Environment = var.environment
     Terraform = true
   }
-  vpc_id = data.aws_ssm_parameter.vpc_id
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
   ami_id = data.aws_ami.ubuntu.id
   private_subnet = split(",",data.aws_ssm_parameter.private_subnet_id.value)[0]
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
