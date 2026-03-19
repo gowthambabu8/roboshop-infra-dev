6cd1645b17355b27b5206a8e52a66bb6ebe19a03
Gowtham <gowthambabu8@gmail.com>
2026-03-19 17:37:58 +0530

    terraform backend module changes

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "frontend_alb" {
-    name = "${var.project}-${var.environment}-alb"
+    name = "${var.project}-${var.environment}-frontend-alb"
     internal = false
     load_balancer_type = "application"
     security_groups = [local.frontend_alb_sg_id] 
