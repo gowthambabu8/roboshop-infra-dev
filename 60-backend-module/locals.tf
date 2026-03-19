48bdc3274224706c3973a958276e260acf8bd6d9
Gowtham <gowthambabu8@gmail.com>
2026-03-19 17:29:01 +0530

    terraform backend module changes

--- 60-backend-module/locals.tf
+++ 60-backend-module/locals.tf
@@ -5,14 +5,4 @@ locals {
     Environment = var.environment
     Terraform = true
   }
-  vpc_id = data.aws_ssm_parameter.vpc_id.value
-  ami_id = data.aws_ami.ubuntu.id
-  subnet_id = split(",",data.aws_ssm_parameter.private_subnet_id.value)[0]
-  sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-  backend_alb_arn = data.aws_ssm_parameter.backend_alb_arn.value
-  component = "catalogue"
-  app_version = "v3"
-  port_number = 8080
-  health_check_path = "/health"
-  domain_name = "happielearning.com"
 }
