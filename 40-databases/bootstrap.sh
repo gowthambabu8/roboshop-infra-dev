bf8356dd491acbf3b6b75afd950abfb8c0b9ca45
Gowtham <gowthambabu8@gmail.com>
2026-03-19 20:51:02 +0530

    terraform backend module changes

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,9 +2,11 @@
 dnf install ansible -y
 
 component=$1
-#environment=$2
+environment=$2
+app_version=$3
+
 cd /home/ec2-user/
 git clone https://github.com/gowthambabu8/roboshop-ansible-roles-tf.git
 cd roboshop-ansible-roles-tf
 git pull
-ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
+ansible-playbook -e component=$component -e environment=$environment -e app_version=$app_version roboshop.yaml
