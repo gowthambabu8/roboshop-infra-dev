2eab4f758c6016d7ef47ef84d29279f08617d603
Gowtham <gowthambabu8@gmail.com>
2026-03-19 23:26:25 +0530

    terraform backend module changes

--- 60-backend-module/variables.tf
+++ 60-backend-module/variables.tf
@@ -19,11 +19,11 @@ variable "components" {
     cart = {
       rule_priority = 30
      },
-  #   shipping = {
-  #     rule_priority = 40
-  #   },
-  #   payment = {
-  #     rule_priority = 50
-  #   }
+    shipping = {
+      rule_priority = 40
+    },
+    payment = {
+      rule_priority = 50
+    }
    }
 }
