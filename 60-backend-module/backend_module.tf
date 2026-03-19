d51a18d11c2982707350178a132e7f0062aeeace
Gowtham <gowthambabu8@gmail.com>
2026-03-19 23:04:50 +0530

    terraform backend module changes

--- 60-backend-module/backend_module.tf
+++ 60-backend-module/backend_module.tf
@@ -1,9 +1,9 @@
-# module "backend" {
-#       for_each = var.components
-#       source = "git::https://github.com/gowthambabu8/terraform-roboshop-component.git?ref=main"
-#       component = each.key
-#       rule_priority = each.value.rule_priority
-#     }
+module "backend" {
+      for_each = var.components
+      source = "git::https://github.com/gowthambabu8/terraform-roboshop-component.git?ref=main"
+      component = each.key
+      rule_priority = each.value.rule_priority
+    }
 # module "catalogue" {
 #   source        = "git::https://github.com/gowthambabu8/terraform-roboshop-component.git?ref=main"
 #   component     = "catalogue"
