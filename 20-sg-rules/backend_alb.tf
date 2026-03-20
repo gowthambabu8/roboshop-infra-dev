2b04948a7d9bd6f0e50d4cecd6bd1b3c2ebe1e3f
Gowtham <gowthambabu8@gmail.com>
2026-03-20 12:29:25 +0530

    terraform backend module changes

--- 20-sg-rules/backend_alb.tf
+++ 20-sg-rules/backend_alb.tf
@@ -15,3 +15,48 @@ resource "aws_security_group_rule" "backend_alb_frontend" {
   source_security_group_id = local.front_sg_id
   security_group_id = local.backend_alb_sg_id
 }
+
+resource "aws_security_group_rule" "backend_alb_catalogue" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  source_security_group_id = local.catalogue_sg_id
+  security_group_id = local.backend_alb_sg_id
+}
+
+resource "aws_security_group_rule" "backend_alb_user" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  source_security_group_id = local.user_sg_id
+  security_group_id = local.backend_alb_sg_id
+}
+
+resource "aws_security_group_rule" "backend_alb_cart" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  source_security_group_id = local.cart_sg_id
+  security_group_id = local.backend_alb_sg_id
+}
+
+resource "aws_security_group_rule" "backend_alb_shipping" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  source_security_group_id = local.shipping_sg_id
+  security_group_id = local.backend_alb_sg_id
+}
+
+resource "aws_security_group_rule" "backend_alb_payment" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  source_security_group_id = local.payment_sg_id
+  security_group_id = local.backend_alb_sg_id
+}
