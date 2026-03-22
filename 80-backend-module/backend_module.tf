94cd35351438275097ea1046573011d298a9d2ae
Gowtham <gowthambabu8@gmail.com>
2026-03-22 12:05:38 +0530

    terraform code changes

--- 80-backend-module/backend_module.tf
+++ 80-backend-module/backend_module.tf
@@ -3,6 +3,7 @@ module "backend" {
       source = "git::https://github.com/gowthambabu8/terraform-roboshop-component.git?ref=main"
       component = each.key
       rule_priority = each.value.rule_priority
+      environment = var.environment
     }
 # module "catalogue" {
 #   source        = "git::https://github.com/gowthambabu8/terraform-roboshop-component.git?ref=main"
