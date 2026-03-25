0e80dceed9b0170e244dbff02cf99a076456a86a
Gowtham <gowthambabu8@gmail.com>
2026-03-25 12:37:51 +0530

    terraform aws sg changes

--- 40-databases/module/mysql/locals.tf
+++ 40-databases/module/mysql/locals.tf
@@ -11,5 +11,5 @@ locals {
   redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
   rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
-
+  account_id = data.aws_caller_identity.current.account_id
 }
