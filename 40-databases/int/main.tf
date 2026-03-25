717fb6b5c54398991ef2eb389c0224146e53fabe
Gowtham <gowthambabu8@gmail.com>
2026-03-25 11:35:26 +0530

    terraform aws sg changes

--- 40-databases/int/main.tf
+++ 40-databases/int/main.tf
@@ -4,3 +4,24 @@ module "mongo" {
     environment = var.environment
     zone_id = var.zone_id
 }
+
+module "redis" {
+    source = "../module/redis"
+    project = var.project
+    environment = var.environment
+    zone_id = var.zone_id
+}
+
+module "mysql" {
+    source = "../module/mysql"
+    project = var.project
+    environment = var.environment
+    zone_id = var.zone_id
+}
+
+module "rabbitmq" {
+    source = "../module/rabbitmq"
+    project = var.project
+    environment = var.environment
+    zone_id = var.zone_id
+}
