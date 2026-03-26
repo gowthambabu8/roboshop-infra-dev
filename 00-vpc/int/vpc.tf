123e6d2a84178b1f59bfb212317a2b3b951e8b62
Gowtham <gowthambabu8@gmail.com>
2026-03-26 13:54:58 +0530

    updated variables in int.tfvars

--- 00-vpc/int/vpc.tf
+++ 00-vpc/int/vpc.tf
@@ -8,13 +8,3 @@
       private_subnet_cidr = var.private_subnet_cidr_blocks
       database_subnet_cidr = var.database_subnet_cidr_blocks
     }
-
-    # module "parameter_store" {
-    #   source = "git::https://github.com/gowthambabu8/terraform-aws-vpc.git//parameter-store"
-    #   project = var.project
-    #   environment = var.environment
-    #   vpc_id = module.vpc.vpc_id
-    #   public_subnet_cidr = var.public_subnet_cidr_blocks
-    #   private_subnet_cidr = var.private_subnet_cidr_blocks
-    #   database_subnet_cidr = var.database_subnet_cidr_blocks
-    # }
--- 60-acm/int/main.tf
+++ 60-acm/int/main.tf
@@ -4,4 +4,6 @@ module "acm" {
   environment = var.environment
   zone_id = var.zone_id
   domain_name = var.domain_name
+  component = var.component
+  app_version = var.app_version
 }
